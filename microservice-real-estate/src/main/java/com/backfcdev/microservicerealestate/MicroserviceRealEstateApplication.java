a4d868ca73c938558033d1055f9ec69b1bde8330
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-05-13 21:45:34 -0500

    first commit

---
+++ microservice-real-estate/src/main/java/com/backfcdev/microservicerealestate/MicroserviceRealEstateApplication.java
@@ -0,0 +1,13 @@
+package com.backfcdev.microservicerealestate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceRealEstateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceRealEstateApplication.class, args);
+	}
+
+}
---
+++ microservice-real-estate/src/test/java/com/backfcdev/microservicerealestate/MicroserviceRealEstateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backfcdev.microservicerealestate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceRealEstateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
