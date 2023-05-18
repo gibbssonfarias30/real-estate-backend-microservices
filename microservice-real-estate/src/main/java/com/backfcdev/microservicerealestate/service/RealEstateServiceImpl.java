2d975529ddeb99f231b05ee4c465c30ed5e1da0d
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-05-18 07:25:42 -0500

    the implementation of the service was carried out with its respective logic

---
+++ microservice-real-estate/src/main/java/com/backfcdev/microservicerealestate/service/RealEstateServiceImpl.java
@@ -0,0 +1,41 @@
+package com.backfcdev.microservicerealestate.service;
+
+import com.backfcdev.microservicerealestate.exception.EntityNotFoundException;
+import com.backfcdev.microservicerealestate.model.RealEstate;
+import com.backfcdev.microservicerealestate.repository.IRealEstateRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+@RequiredArgsConstructor
+@Service
+public class RealEstateServiceImpl implements IRealEstateService{
+
+    private final IRealEstateRepository realEstateRepository;
+
+
+    @Override
+    public List<RealEstate> findAll() {
+        return realEstateRepository.findAll();
+    }
+
+    @Override
+    public RealEstate save(RealEstate realEstate) {
+        realEstate.setCreatedDate(LocalDateTime.now());
+        return realEstateRepository.save(realEstate);
+    }
+
+    @Override
+    public RealEstate findById(Long id) {
+        return realEstateRepository.findById(id)
+                .orElseThrow(EntityNotFoundException::new);
+    }
+
+    @Override
+    public void delete(Long id) {
+        realEstateRepository.deleteById(id);
+    }
+}
