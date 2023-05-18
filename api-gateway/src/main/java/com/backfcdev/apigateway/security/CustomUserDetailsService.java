f2c9bcba2ba8c0568685a6bb07d180a93f39e6ef
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-05-18 12:00:46 -0500

    CustomUserDetailsService was created implementing UserDetailsService to retrieve data related to the user, in addition to its loadUserByUsername() method that returns a user's information based on their username

---
+++ api-gateway/src/main/java/com/backfcdev/apigateway/security/CustomUserDetailsService.java
@@ -0,0 +1,39 @@
+package com.backfcdev.apigateway.security;
+
+
+import com.backfcdev.apigateway.model.User;
+import com.backfcdev.apigateway.service.IUserService;
+import com.backfcdev.apigateway.util.SecurityUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final IUserService userService;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("The user was not found: " + username));
+
+        Set<GrantedAuthority> authorities = Set.of(SecurityUtils.convertToAuthority(user.getRole().name()));
+
+        return UserPrincipal.builder()
+                .user(user)
+                .id(user.getId())
+                .username(username)
+                .password(user.getPassword())
+                .authorities(authorities)
+                .build();
+    }
+}
