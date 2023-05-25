a5dcd03d23da8bd13d90c7779fc4c4aea35a045c
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-05-25 12:07:34 -0500

    the necessary methods for the generation and validation of JWT tokens were defined

---
+++ api-gateway/src/main/java/com/backfcdev/apigateway/security/jwt/JwtProvider.java
@@ -0,0 +1,13 @@
+package com.backfcdev.apigateway.security.jwt;
+
+import com.backfcdev.apigateway.security.UserPrincipal;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.core.Authentication;
+
+public interface JwtProvider {
+    String generateToken(UserPrincipal auth);
+
+    Authentication getAuthentication(HttpServletRequest request);
+
+    boolean isTokenValid(HttpServletRequest request);
+}
