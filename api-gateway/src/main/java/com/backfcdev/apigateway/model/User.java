58783f89f3fad76e167ef3879574e10e44056825
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-05-25 12:01:27 -0500

    the empty constructor was added, another with parameters and the token attribute with the @Transient annotation

--- api-gateway/src/main/java/com/backfcdev/apigateway/model/User.java
+++ api-gateway/src/main/java/com/backfcdev/apigateway/model/User.java
@@ -2,14 +2,18 @@ package com.backfcdev.apigateway.model;
 
 import com.backfcdev.apigateway.enums.Role;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name="users")
 public class User {
 
@@ -32,5 +36,8 @@ public class User {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role;
+
+    @Transient
+    private String token;
 
 }
