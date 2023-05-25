697c69bb32a895f7ceaf4eb4ffb1068561f91c0d
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-05-25 12:03:55 -0500

    the AUTH_HEADER constant was modified

--- api-gateway/src/main/java/com/backfcdev/apigateway/util/SecurityUtils.java
+++ api-gateway/src/main/java/com/backfcdev/apigateway/util/SecurityUtils.java
@@ -9,7 +9,7 @@ import org.springframework.util.StringUtils;
 @Component
 public class SecurityUtils {
     public static final String ROLE_PREFIX = "ROLE_";
-    public static final String AUTH_HEADER = "authorization";
+    public static final String AUTH_HEADER = "Authorization";
     public static final String AUTH_TOKEN_TYPE = "Bearer";
     public static final String AUTH_TOKEN_PREFIX = AUTH_TOKEN_TYPE + " ";
 
