0e8af6f1d94582895fdb4dc75d91a7d13ddb727e
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-05-26 07:44:40 -0500

    defines methods for interacting with a real estate web service using Feign. Each method has an annotation indicating the type of HTTP request to make and the parameters required to complete the request.

---
+++ api-gateway/src/main/java/com/backfcdev/apigateway/request/IRealEstateServiceRequest.java
@@ -0,0 +1,25 @@
+package com.backfcdev.apigateway.request;
+
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@FeignClient(
+        value = "real-estate-service",
+        path = "/api/v1/real-estate",
+        url = "${real-estate.service.url}",
+        configuration = FeignConfiguration.class
+)
+public interface IRealEstateServiceRequest {
+
+    @PostMapping
+    Object saveRealEstate(@RequestBody Object requestBody);
+
+    @DeleteMapping("/{realEstateId}")
+    void deleteRealEstate(@PathVariable Long realEstateId);
+
+    @GetMapping
+    List<Object> getAllRealEstates();
+}
