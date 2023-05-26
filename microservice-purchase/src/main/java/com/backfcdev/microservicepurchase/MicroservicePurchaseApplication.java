5c5d0a28ed15a01bd80530faf09c962e9b5999b6
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-05-26 09:46:52 -0500

    enable eureka discovery client

--- microservice-purchase/src/main/java/com/backfcdev/microservicepurchase/MicroservicePurchaseApplication.java
+++ microservice-purchase/src/main/java/com/backfcdev/microservicepurchase/MicroservicePurchaseApplication.java
@@ -2,7 +2,9 @@ package com.backfcdev.microservicepurchase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MicroservicePurchaseApplication {
 
--- microservice-real-estate/src/main/java/com/backfcdev/microservicerealestate/MicroserviceRealEstateApplication.java
+++ microservice-real-estate/src/main/java/com/backfcdev/microservicerealestate/MicroserviceRealEstateApplication.java
@@ -2,7 +2,9 @@ package com.backfcdev.microservicerealestate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MicroserviceRealEstateApplication {
 
