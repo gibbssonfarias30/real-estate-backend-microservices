f3f650537297b29df459b66656e10f7c4d68bd31
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-05-26 08:20:37 -0500

    defines methods to interact with a service purchases using Feign.

---
+++ api-gateway/src/main/java/com/backfcdev/apigateway/request/IPurchaseServiceRequest.java
@@ -0,0 +1,24 @@
+package com.backfcdev.apigateway.request;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@FeignClient(
+        value = "purchase-service",
+        path = "/api/v1/purchase",
+        url = "${purchase.service.url}",
+        configuration = FeignConfiguration.class
+)
+public interface IPurchaseServiceRequest {
+
+    @PostMapping
+    Object savePurchase(@RequestBody Object requestBody);
+
+    @GetMapping("/{userId}")
+    List<Object> getAllPurchasesOfUser(@PathVariable Long userId);
+
+
+}
