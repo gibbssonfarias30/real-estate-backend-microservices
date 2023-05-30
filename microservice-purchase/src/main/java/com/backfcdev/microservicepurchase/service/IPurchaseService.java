8c62989fc6c437b0f822e57779d502ac9db15755
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-05-30 11:10:39 -0500

    added methods CRUD

--- microservice-purchase/src/main/java/com/backfcdev/microservicepurchase/service/IPurchaseService.java
+++ microservice-purchase/src/main/java/com/backfcdev/microservicepurchase/service/IPurchaseService.java
@@ -6,6 +6,15 @@ import java.util.List;
 
 public interface IPurchaseService {
 
+    List<Purchase> findAll();
+
     Purchase save(Purchase purchase);
+
+    Purchase findById(Long id);
+
+    Purchase update(Long id, Purchase purchase);
+
+    void delete(Long id);
+
     List<Purchase> findAllPurchasesOfUser(Long userId);
 }
