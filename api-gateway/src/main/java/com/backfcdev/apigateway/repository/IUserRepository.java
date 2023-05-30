4a0fd70d2eaae2f038085592f4f65126ed553c36
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-05-30 12:01:49 -0500

    an existsByUsername method was made in the repository

--- api-gateway/src/main/java/com/backfcdev/apigateway/repository/IUserRepository.java
+++ api-gateway/src/main/java/com/backfcdev/apigateway/repository/IUserRepository.java
@@ -16,4 +16,6 @@ public interface IUserRepository extends JpaRepository<User, Long> {
     @Modifying
     @Query("update User set role=:role WHERE username=:username")
     void updateUserRole(@Param("username") String username, @Param("role") Role role);
+
+    boolean existsByUsername(String username);
 }
