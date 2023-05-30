1a3ee6ea1097c0a7fd1f3a229922f9813cf78b0a
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-05-30 11:11:10 -0500

    added impl logic in the methods

--- microservice-purchase/src/main/java/com/backfcdev/microservicepurchase/service/PurchaseServiceImpl.java
+++ microservice-purchase/src/main/java/com/backfcdev/microservicepurchase/service/PurchaseServiceImpl.java
@@ -2,6 +2,7 @@ package com.backfcdev.microservicepurchase.service;
 
 import com.backfcdev.microservicepurchase.model.Purchase;
 import com.backfcdev.microservicepurchase.repository.IPurchaseRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,9 +18,34 @@ public class PurchaseServiceImpl implements IPurchaseService{
 
 
     @Override
+    public List<Purchase> findAll() {
+        return purchaseRepository.findAll();
+    }
+
+    @Override
     public Purchase save(Purchase purchase) {
         purchase.setPurchaseDate(LocalDateTime.now());
         return purchaseRepository.save(purchase);
+    }
+
+    @Override
+    public Purchase findById(Long id) {
+        return purchaseRepository.findById(id)
+                .orElseThrow(EntityNotFoundException::new);
+    }
+
+    @Override
+    public Purchase update(Long id, Purchase purchase) {
+        purchaseRepository.findById(id)
+                .orElseThrow(EntityNotFoundException::new);
+        return purchaseRepository.save(purchase);
+    }
+
+    @Override
+    public void delete(Long id) {
+        purchaseRepository.findById(id)
+                .orElseThrow(EntityNotFoundException::new);
+        purchaseRepository.deleteById(id);
     }
 
     @Override
