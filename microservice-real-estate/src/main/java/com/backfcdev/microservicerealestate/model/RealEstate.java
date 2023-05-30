900166d8ceff6ace11711f1821397ca3004288d2
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-05-30 10:27:08 -0500

    properties of certain attributes were removed to be placed in the dto

--- microservice-real-estate/src/main/java/com/backfcdev/microservicerealestate/model/RealEstate.java
+++ microservice-real-estate/src/main/java/com/backfcdev/microservicerealestate/model/RealEstate.java
@@ -15,18 +15,14 @@ public class RealEstate {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 150, nullable = false)
     private String name;
 
-    @Column(length = 500, nullable = false)
     private String direction;
 
-    @Column(length = 1200)
     private String picture;
 
-    @Column(nullable = false)
     private Double price;
 
-    @Column(name = "created_date", nullable = false)
+    @Column(name = "created_date")
     private LocalDateTime createdDate;
 }
