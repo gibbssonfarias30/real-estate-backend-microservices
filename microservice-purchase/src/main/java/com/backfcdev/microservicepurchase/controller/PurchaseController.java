b2eba47770e796c6981dea94c7097dd4f227f34b
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-05-30 11:11:21 -0500

    the mappers were used in the methods

--- microservice-purchase/src/main/java/com/backfcdev/microservicepurchase/controller/PurchaseController.java
+++ microservice-purchase/src/main/java/com/backfcdev/microservicepurchase/controller/PurchaseController.java
@@ -1,9 +1,11 @@
 package com.backfcdev.microservicepurchase.controller;
 
+import com.backfcdev.microservicepurchase.dto.PurchaseDTO;
 import com.backfcdev.microservicepurchase.model.Purchase;
 import com.backfcdev.microservicepurchase.service.IPurchaseService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -18,18 +20,57 @@ import java.util.List;
 public class PurchaseController {
 
     private final IPurchaseService purchaseService;
+    private final ModelMapper mapper;
+
+
+    @GetMapping
+    ResponseEntity<List<PurchaseDTO>> findAll(){
+        return ResponseEntity.ok(purchaseService.findAll()
+                .stream()
+                .map(this::convertToDto)
+                .toList());
+    }
 
     @PostMapping
-    ResponseEntity<Void> save(@RequestBody Purchase purchase){
-        Purchase purchaseCreated = purchaseService.save(purchase);
+    ResponseEntity<Void> save(@RequestBody PurchaseDTO dto){
+        Purchase purchase = purchaseService.save(convertToEntity(dto));
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
-                .path("/{id}").buildAndExpand(purchaseCreated.getId()).toUri();
+                .path("/{id}").buildAndExpand(purchase.getId()).toUri();
         log.info("purchase created {}", purchase.getTitle());
         return ResponseEntity.created(location).build();
     }
 
-    @GetMapping("/{userId}")
-    ResponseEntity<List<Purchase>> getAllPurchasesOfUser(@PathVariable Long userId){
-        return ResponseEntity.ok(purchaseService.findAllPurchasesOfUser(userId));
+    @GetMapping("/{id}")
+    ResponseEntity<PurchaseDTO> findById(@PathVariable Long id){
+        return ResponseEntity.ok(convertToDto(purchaseService.findById(id)));
+    }
+
+    @PutMapping("/{id}")
+    ResponseEntity<PurchaseDTO> update(@PathVariable Long id, @RequestBody PurchaseDTO dto ){
+        return ResponseEntity.ok(convertToDto(purchaseService.update(id, convertToEntity(dto))));
+    }
+
+    @DeleteMapping("/{id}")
+    ResponseEntity<?> delete(@PathVariable Long id){
+        purchaseService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/users/{userId}")
+    ResponseEntity<List<PurchaseDTO>> getAllPurchasesOfUser(@PathVariable Long userId){
+        return ResponseEntity.ok(purchaseService.findAllPurchasesOfUser(userId)
+                .stream()
+                .map(this::convertToDto)
+                .toList());
+    }
+
+
+
+    private PurchaseDTO convertToDto(Purchase entity){
+        return mapper.map(entity, PurchaseDTO.class);
+    }
+
+    private Purchase convertToEntity(PurchaseDTO dto){
+        return mapper.map(dto, Purchase.class);
     }
 }
