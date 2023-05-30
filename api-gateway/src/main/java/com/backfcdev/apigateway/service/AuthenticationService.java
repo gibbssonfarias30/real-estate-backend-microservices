dc7fb0fc85cfce7f1970c0191402b9ca3c968f9e
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-05-30 11:57:36 -0500

    remove name interface

--- api-gateway/src/main/java/com/backfcdev/apigateway/service/AuthenticationService.java
+++
@@ -1,7 +0,0 @@
-package com.backfcdev.apigateway.service;
-
-import com.backfcdev.apigateway.model.User;
-
-public interface AuthenticationService {
-    User signInAndReturnJWT(User signInRequest);
-}
